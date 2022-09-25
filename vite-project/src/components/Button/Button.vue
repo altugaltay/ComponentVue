c3ea02253b57f612a869d7e555e442365f8d2dc3
Altuğ Altay <106876176+altugaltay@users.noreply.github.com>
2022-09-25 17:39:20 +0300

    added

---
+++ vite-project/src/components/Button/Button.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import { onMounted } from "vue";
+    const props = defineProps(['name']);
+    const emit = defineEmits(['setName']);
+    const nameHandler = name => {
+      emit('setName', name);
+    }
+    onMounted(() => console.log(`Button onMounted`, '👻'));
+    </script>
+    
+    <template>
+      <button class="button" :class="name.toLowerCase()" @click="nameHandler(props.name)">{{ props.name }}</button>
+    </template>
+    
+    <style scoped>
+    .button {
+      margin: 10px;
+      padding: 12px 0;
+      min-width: 120px;
+      background-color: transparent;
+      border: 5px solid rgb(37, 37, 37);
+      cursor: pointer;
+    }
+    .orchid {
+      background-color: orchid;
+    }
+    .steelblue {
+      background-color: steelblue;
+    }
+    .indianred {
+      background-color: indianred;
+    }
+    .yellowgreen {
+      background-color: yellowgreen;
+    }
+    .lightseagreen {
+      background-color: lightseagreen;
+    }
+    </style>
---
+++ vite-project/src/components/Card/CardItem.vue
@@ -0,0 +1,34 @@
+<script setup>
+    import { onMounted } from "vue";
+    const props = defineProps(['item']);
+    onMounted(() => console.log(`CardItem onMounted`, `🔥`)); 
+    </script>
+    
+    <template>
+      <li class="card__item" :class="{'-assitant': props.item.type}">{{props.item.name}}</li>
+    </template>
+    
+    <style>
+    .card__item {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+      margin: 10px 0;
+      padding: 4px 0 4px 20px;
+      border-bottom: 6px solid rgb(37, 37, 37);
+      border-left: 10px solid rgb(37, 37, 37);
+      text-align: left;
+      list-style: none;
+    }
+    .card__item.-assitant {
+      order: -1;
+      background: black;
+      color: white;
+    }
+    .card__item.-assitant::before {
+      content: '🦄';
+      display: block;
+      padding-right: 10px;
+    }
+    </style>
